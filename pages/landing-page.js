c07f0150fa8e310de6745fed691ad1b14f4d195f
shakyakarunathilake <shakyakarunathilake>
2023-07-22 14:46:39 +0000

    First Commit from Locofy Builder

---
+++ pages/landing-page.js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import Header from "../components/header";
+import Hero from "../components/hero";
+import AreaContainer from "../components/area-container";
+import FeatureContainer from "../components/feature-container";
+import RentPropertiesContainer from "../components/rent-properties-container";
+import RentPropertiesForm from "../components/rent-properties-form";
+import Contact from "../components/contact";
+import FooterSection from "../components/footer-section";
+
+const LandingPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Real Estate</title>
+        <meta name="description" content="Discover your perfect home" />
+      </Head>
+      <div className="relative bg-gray-white w-full h-[5095.99px] flex flex-col items-center justify-start">
+        <Header hamburger={false} />
+        <Hero />
+        <AreaContainer />
+        <FeatureContainer />
+        <RentPropertiesContainer />
+        <RentPropertiesForm />
+        <Contact />
+        <FooterSection
+          houseLine="/houseline2.svg"
+          socialMediaLogo="/social-media-logo5.svg"
+          socialMediaLogo1="/social-media-logo6.svg"
+          socialMediaLogo2="/social-media-logo7.svg"
+          socialMediaLogo3="/social-media-logo8.svg"
+          socialMediaLogo4="/social-media-logo9.svg"
+          propOverflow="unset"
+        />
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
